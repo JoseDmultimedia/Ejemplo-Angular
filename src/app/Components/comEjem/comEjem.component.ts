016cfce12d709230115acdc0635972922b1da23c
joseDmultimedia <jose_dav.garcia@uao.edu.co>
2021-09-04 20:28:52 -0500

    Entendiendo los compoenentes en Angular, creación manual

---
+++ src/app/Components/comEjem/comEjem.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-comEjem',
+    templateUrl: './comEjem.component.html',
+    styleUrls: ['./comEjem.component.css']
+})
+
+export class comEjemComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { comEjemComponent } from './Components/comEjem/comEjem.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    comEjemComponent
   ],
   imports: [
     BrowserModule,
